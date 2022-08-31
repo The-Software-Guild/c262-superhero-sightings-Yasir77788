c7fb74127303e53cc37b13b4ea01f0f0309553a9
Yasir_Hassan <yasir.hassan7722@gmail.com>
2022-08-31 15:28:51 -0500

    add LocationController

--- SuperheroSightings/src/main/java/com/sg/superheroSightings/controllers/LocationController.java
+++ SuperheroSightings/src/main/java/com/sg/superheroSightings/controllers/LocationController.java
@@ -5,11 +5,14 @@ import com.sg.superheroSightings.dao.OrganizationDao;
 import com.sg.superheroSightings.dao.SightingDao;
 import com.sg.superheroSightings.dao.SuperDao;
 import com.sg.superheroSightings.dto.Location;
+import com.sg.superheroSightings.dto.Super;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -17,22 +20,76 @@ public class LocationController {
     @Autowired
     private LocationDao locationDao;
 
-    @Autowired
-    private SuperDao superDao;
+//    @Autowired
+//    private SuperDao superDao;
+//
+//    @Autowired
+//    private OrganizationDao orgDao;
+//
+//    @Autowired
+//    private SightingDao sightingDao;
 
-    @Autowired
-    private OrganizationDao orgDao;
-
-    @Autowired
-    private SightingDao sightingDao;
-
-    @GetMapping("locations")
-    public String displayLocations(Model model) {
-        List<Location> locations = locationDao.getAllLocations();
-        model.addAttribute("locations", locations);
-        return "locations";
+    @GetMapping("supers")
+    public String displaySuper(Model model) {
+        List<Super> supers = superDao.getAllSupers();
+        model.addAttribute("supers", supers);
+        return "supers";
     }
 
 
+//    @PostMapping("addSuper")
+//    public String addSuper(HttpServletRequest request) {
+//        String superName = request.getParameter("superName");
+//        String superDescription = request.getParameter("superDescription");
+//        String superPower = request.getParameter("superPower");
+//        String superStatus = request.getParameter("superStatus");
+//
+//        Super sp = new Super();
+//        sp.setSuperName(superName);
+//        sp.setSuperDescription(superDescription);
+//        sp.setSuperPower(superPower);
+//        sp.setSuperStatus(superStatus);
+//
+//        superDao.addSuper(sp);
+//
+//        return "redirect:/supers";
+//    }
+//
+//
+//    @GetMapping("deleteSuper")
+//    public String deleteSuper(HttpServletRequest request) {
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        superDao.deleteSuperById(id);
+//
+//        return "redirect:/supers";
+//    }
+//
+//    @GetMapping("editSuper")
+//    public String editSuper(HttpServletRequest request, Model model) {
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        Super superObj = superDao.getSuperById(id);
+//
+//        model.addAttribute("super", superObj);
+//        return "editSuper";
+//    }
+//
+//    @PostMapping("editSuper")
+//    public String performEditSuper(HttpServletRequest request) {
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        Super superObj = superDao.getSuperById(id);
+//
+//        superObj.setSuperName(request.getParameter("superName"));
+//        superObj.setSuperDescription(request.getParameter("superDescription"));
+//        superObj.setSuperPower(request.getParameter("superPower"));
+//        superObj.setSuperStatus(request.getParameter("superStatus"));
+//
+//        superDao.updateSuper(superObj);
+//
+//        return "redirect:/supers";
+//    }
+//
+//
+//
+//
 
 }
