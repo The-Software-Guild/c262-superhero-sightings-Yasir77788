71fa096d27aeb6d189ad5a7bd68c01be9fe54179
Yasir_Hassan <yasir.hassan7722@gmail.com>
2022-09-06 19:28:41 -0500

    update SightingDaoDBImplTest class

--- SuperheroSightings/src/test/java/com/sg/superheroSightings/dao/SightingDaoDBImplTest.java
+++ SuperheroSightings/src/test/java/com/sg/superheroSightings/dao/SightingDaoDBImplTest.java
@@ -239,4 +239,147 @@ public class SightingDaoDBImplTest {
         assertNull(fromDAO);
     }
 
+    @Test
+    public void testGetRecentSightings() {
+        Location loc1 = new Location();
+        loc1.setLocationName("XYZ");
+        loc1.setLocationDescription("Good Place");
+        loc1.setStreet("1234 st");
+        loc1.setCity("Fair city");
+        loc1.setState("ST");
+        loc1.setZipCode("12345");
+        loc1.setLocationLat("123--1f-23");
+        loc1.setLocationLong("321-dx-211");
+        locationDao.addLocation(loc1);
+
+        Super super1 = new Super();
+        super1.setSuperName("Test Spiderman");
+        super1.setSuperDescription("A man that is a spider");
+        super1.setSuperPower("Can climb high buildings");
+        super1.setSuperStatus("Hero");
+        superDao.addSuper(super1);
+
+        Sighting sighting1 = new Sighting();
+        LocalDate date1 = LocalDate.of(2022, 8, 30);
+        sighting1.setSightingDate(date1);
+        sighting1.setSuperObj(super1);
+        sighting1.setLocation(loc1);
+        sighting1 = sightingDao.addSighting(sighting1);
+
+        Location loc2 = new Location();
+        loc2.setLocationName("City Hospital2");
+        loc2.setLocationDescription("The Hospital on the stree");
+        loc2.setStreet("1234 Best Street");
+        loc2.setCity("Good city");
+        loc2.setState("SC");
+        loc2.setZipCode("12345");
+        loc2.setLocationLat("123-cx-123");
+        loc2.setLocationLong("341-mc-291");
+        locationDao.addLocation(loc2);
+
+        Super super2 = new Super();
+        super2.setSuperName("Test Spiderman2");
+        super2.setSuperDescription("A man that is a spider");
+        super2.setSuperPower("Can climb");
+        super2.setSuperStatus("Villain");
+        superDao.addSuper(super2);
+
+        Sighting sighting2 = new Sighting();
+        LocalDate date2 = LocalDate.of(2022, 8, 30);
+        sighting2.setSightingDate(date2);
+        sighting2.setSuperObj(super1);
+        sighting2.setLocation(loc1);
+        sighting2 = sightingDao.addSighting(sighting2);
+
+        List<Sighting> sightinglist = sightingDao.getMostRecentSightings();
+
+        assertEquals(2, sightinglist.size());
+        assertTrue(sightinglist.contains(sighting1));
+        assertTrue(sightinglist.contains(sighting2));
+    }
+
+    @Test
+    public void testGetSightingsForSuper() throws ParseException {
+        Location location = new Location();
+        location.setLocationName("Music Corner");
+        location.setLocationDescription("The location on the corner");
+        location.setStreet("1234 First Street");
+        location.setCity("Music city");
+        location.setState("MC");
+        location.setZipCode("89098");
+        location.setLocationLat("123-ac-487");
+        location.setLocationLong("342-da-873");
+        locationDao.addLocation(location);
+
+        Super superObj = new Super();
+        superObj.setSuperName("Test Spider-man");
+        superObj.setSuperDescription("A man that is a spider");
+        superObj.setSuperPower("Can climb");
+        superObj.setSuperStatus("Hero");
+        superDao.addSuper(superObj);
+
+        Sighting sighting = new Sighting();
+        LocalDate date1 = LocalDate.of(2022, 8, 30);
+        sighting.setSightingDate(date1);
+        sighting.setSuperObj(superObj);
+        sighting.setLocation(location);
+        sighting = sightingDao.addSighting(sighting);
+
+        Sighting sighting2 = new Sighting();
+        LocalDate date2 = LocalDate.of(2022, 8, 30);
+        sighting2.setSightingDate(date2);
+        sighting2.setSuperObj(superObj);
+        sighting2.setLocation(location);
+        sighting2 = sightingDao.addSighting(sighting2);
+
+        List<Sighting> listOfSighting = sightingDao.getSightingForSuper(superObj);
+
+        assertEquals(2, listOfSighting.size());
+        assertTrue(listOfSighting.contains(sighting2));
+
+    }
+
+
+    @Test
+    public void testGetSightingsForLocation() throws ParseException {
+        Location location = new Location();
+        location.setLocationName("Music Corner");
+        location.setLocationDescription("The location on the corner");
+        location.setStreet("1234 First Street");
+        location.setCity("Music city");
+        location.setState("MC");
+        location.setZipCode("89098");
+        location.setLocationLat("123-ac-487");
+        location.setLocationLong("342-da-873");
+        locationDao.addLocation(location);
+
+        Super superObj = new Super();
+        superObj.setSuperName("Test Spider-man");
+        superObj.setSuperDescription("A man that is a spider");
+        superObj.setSuperPower("Can climb");
+        superObj.setSuperStatus("Hero");
+        superDao.addSuper(superObj);
+
+        Sighting sighting = new Sighting();
+        LocalDate date1 = LocalDate.of(2022, 8, 30);
+        sighting.setSightingDate(date1);
+        sighting.setSuperObj(superObj);
+        sighting.setLocation(location);
+        sighting = sightingDao.addSighting(sighting);
+
+        Sighting sighting2 = new Sighting();
+        LocalDate date2 = LocalDate.of(2022, 8, 30);
+        sighting2.setSightingDate(date2);
+        sighting2.setSuperObj(superObj);
+        sighting2.setLocation(location);
+        sighting2 = sightingDao.addSighting(sighting2);
+
+        List<Sighting> listOfSighting = sightingDao.getSightingsForLocation(location);
+
+        assertEquals(2, listOfSighting.size());
+        assertTrue(listOfSighting.contains(sighting2));
+
+    }
+
+
 }
